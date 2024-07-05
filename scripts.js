05c6c32cdaaeb7644b91287f2a236653037fe795
Daniel Sagua <danielsagua.n@gmail.com>
2024-07-04 20:20:50 -0400

    v2uno

--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Página cargada');
-    // Aquí puedes agregar más funcionalidades con JavaScript si es necesario
+
+    // Modal de producto
+    var productModal = document.getElementById('productModal');
+    productModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget;
+        var producto = JSON.parse(button.getAttribute('data-producto'));
+        
+        var modalNombre = productModal.querySelector('#modalNombre');
+        var modalSku = productModal.querySelector('#modalSku');
+        var modalDescripcion = productModal.querySelector('#modalDescripcion');
+        var modalPrecio = productModal.querySelector('#modalPrecio');
+        var modalImagen = productModal.querySelector('#modalImagen');
+        
+        modalNombre.textContent = producto.nombre;
+        modalSku.textContent = 'SKU: ' + producto.sku;
+        modalDescripcion.textContent = producto.descripcion;
+        modalPrecio.textContent = producto.precio;
+        modalImagen.src = producto.imagen;
+        modalImagen.alt = producto.nombre;
+    });
 });
